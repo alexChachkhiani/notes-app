2b0f84d222a78d486ce4a7fb2cb4e5b9201be083
alexOrdnas <devfterr@gmail.com>
2024-01-22 00:34:41 +0400

    added functionality to a searchbar icon

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { React, useRef } from 'react'
 import { MdSearch } from 'react-icons/md'
 
 export default function Search({ handleSearchNote }) {
+  const ref = useRef(null);
+
+  function handleClick() {
+    ref.current.focus()
+  }
+
   return (
     <div className="search-bar">
-        <MdSearch className="search-icons" size="2.5em" /> 
-        <input onChange={(event) => handleSearchNote(event.target.value)} type="text" placeholder="Type to Search..." />
+        <MdSearch onClick={handleClick} className="search-icons search-bar--icon" size="2.5em" /> 
+        <input ref={ref} onChange={(event) => handleSearchNote(event.target.value)} type="text" placeholder="Type to Search..." />
     </div>
   )
 }
