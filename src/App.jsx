cd43b491cf647607d4159a74dfa919fea8866b99
alexOrdnas <devfterr@gmail.com>
2024-01-21 18:29:06 +0400

    now you can delete a note

--- src/App.jsx
+++ src/App.jsx
@@ -27,20 +27,26 @@ function App() {
     }
   ])
 
-  const addNote = (text) => {
+  function addNote(text) {
     const date = new Date();
     const newNote = {
       id: nanoid(),
       text: text,
       date: date.toLocaleDateString()
     }
+
     const newNotes = [...notes, newNote]
     setNotes(newNotes)
+  }
+
+  function deleteNote(id) {
+    const newNotes = notes.filter((note) => note.id !== id)
+    setNotes(newNotes);
   }
   
   return (
     <div className="container">
-      <NotesList notes={notes} handleAddNote={addNote} />
+      <NotesList notes={notes} handleAddNote={addNote} handleDeleteNote={deleteNote} />
     </div>
   )
 }
--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import DeleteIcon from '../assets/delete-icon.png'
 
-export default function Note({ id, text, date }) {
+export default function Note({ id, text, date, handleDeleteNote }) {
   console.log(text)
   return (
     <div className="note">
         <p>{text}</p>
         <div className="note-footer">
             <small>{date}</small>
-            <img src={DeleteIcon} alt="Delete Icon" className="delete-icon" width="20px"/>
+            <img src={DeleteIcon} alt="Delete Icon" className="delete-icon" width="20px" onClick={() => handleDeleteNote(id)} />
         </div>
     </div>
   )
